6639c40a2a103c901ad87adcb17932e17401d92d
Raymond <Raymondobinna92@gmail.com>
2022-09-11 09:36:50 -0700

    i

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
-
+/**
+ * main prints - programming is like a multilingual puzzle
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
+	int i
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
